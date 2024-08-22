52e066892795d52a769c063899f13a079574bfc0
Bo Zhou <2571442z@student.gla.ac.uk>
2024-08-22 18:41:43 +0800

    Create UI frame for Game, Login and Register

---
+++ src/ui/GameJFrame.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class GameJFrame extends JFrame {
+    // 游戏的主界面
+    public GameJFrame(){
+        this.setSize(603,680);
+        this.setVisible(true);
+    }
+}
---
+++ src/ui/RegisterJFrame.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame(){
+        this.setSize(488,480);
+        this.setVisible(true); 
+    }
+}
